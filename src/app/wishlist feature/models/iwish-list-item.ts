a0f09776f47fe45ef034b8f0602b3d6d76731bca
Manar568 <manarabdelhak186@gmail.com>
2026-06-06 03:51:52 -0700

    Add wishlist service and models

--- src/app/wishlist feature/models/iwish-list-item.ts
+++ src/app/wishlist feature/models/iwish-list-item.ts
@@ -1,2 +1,10 @@
 export interface IWishListItem {
+ id: string;
+  productId: string;
+  titleEn: string;
+  titleAr: string;
+  price: number;
+  discountPrice?: number;
+  imageUrl?: string;
+  quantity: number;
 }
--- src/app/wishlist feature/services/wishlist-service.ts
+++ src/app/wishlist feature/services/wishlist-service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URLS } from '../../constants/API_URLS';
+import { IWishList } from '../models/iwish-list';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishlistService {
-  
+  private http = inject(HttpClient);
+
+  getWishList(): Observable<IWishList> {
+    return this.http.get<IWishList>(API_URLS.getWishList, {
+      withCredentials: true,
+    });
+  }
+
+  addItem(productId: string): Observable<IWishList> {
+    return this.http.post<IWishList>(
+      API_URLS.addToWishList,
+      { productId },
+      { withCredentials: true }
+    );
+  }
+
+  removeItem(productId: string): Observable<IWishList> {
+    return this.http.delete<IWishList>(
+      API_URLS.removeFromWishList(productId),
+      { withCredentials: true }
+    );
+  }
 }
