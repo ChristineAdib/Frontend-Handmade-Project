e308e2b12425baadcadc6e60eb5f5d9f8dda63f0
ChristineAdib <christineadib8@gmail.com>
2026-06-16 18:46:07 +0300

    update otp

--- src/app/auth/components/auth.component/otpComponent/otp.component/otp.component.ts
+++ src/app/auth/components/auth.component/otpComponent/otp.component/otp.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnDestroy } from '@angular/core';
+import { Component, inject, signal, OnDestroy, computed } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -19,8 +19,17 @@ export class OtpComponent implements OnDestroy {
   protected readonly langService = inject(LanguageService);
 
   isLoading = signal(false);
-  otpTimer  = signal(60);
+  otpTimer  = signal(600);
   canResend = signal(false);
+
+  formattedTimer = computed(() => {
+    const totalSecs = this.otpTimer();
+    const mins = Math.floor(totalSecs / 60);
+    const secs = totalSecs % 60;
+    const padMins = mins.toString().padStart(2, '0');
+    const padSecs = secs.toString().padStart(2, '0');
+    return `${padMins}:${padSecs}`;
+  });
   private timerInterval: any;
 
   otpForm = this.fb.group({
@@ -58,7 +67,7 @@ export class OtpComponent implements OnDestroy {
     if (!this.canResend()) return;
     this.auth.resendOtp({ email: this.auth.otpEmail() }).subscribe({
       next: () => {
-        this.otpTimer.set(60);
+        this.otpTimer.set(600);
         this.canResend.set(false);
         this.startOtpTimer();
       }
