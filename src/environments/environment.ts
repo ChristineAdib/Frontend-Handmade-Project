b6407e9a1656c1b04704136f35115c2aebc5dd8d
ChristineAdib <christineadib8@gmail.com>
2026-06-29 15:12:01 +0300

    add env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5216',
-  domain: 'http://localhost:5216',  // ← أضيفي السطر ده
+  apiUrl: 'https://handauraa.runasp.net',
+  domain: 'https://handauraa.runasp.net',  // ← أضيفي السطر ده
   googleClientId: '374544332234-8gjo6ikhetur8pjb3aohgkfj5sejo4fk.apps.googleusercontent.com',
   demoMode: false
 };
