ebd50d1a9f307f84356c0858983b75ade3483022
ChristineAdib <christineadib8@gmail.com>
2026-07-01 11:45:54 +0300

    http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://handauraa.runasp.net',
-  domain: 'https://handauraa.runasp.net', 
+  apiUrl: 'http://handauraa.runasp.net',
+  domain: 'http://handauraa.runasp.net', 
   googleClientId: '374544332234-8gjo6ikhetur8pjb3aohgkfj5sejo4fk.apps.googleusercontent.com',
   demoMode: false,
   authMode: 'bearer' as 'cookie' | 'bearer'
